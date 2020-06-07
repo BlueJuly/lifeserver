1b461d5e2902e37716453ce4a3bc9a1f56c3e0a6
Xiaochuan Dai <xiaochuandai@Xiaochuans-MacBook-Pro.local>
2020-06-07 17:28:41 -0400

    add some test data

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-
+const userObejct = {
+  userName: 'Leon',
+  authToken: 'faketoken1949',
+  address: '五道口职业技术学院',
+  age:18
+};
 router.get("/", (req, res) => {
-  res.send({ response: "For Oliver Test" }).status(200);
+  res.send(userObejct).status(200);
 });
 
 module.exports = router;
